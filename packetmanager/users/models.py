0dbce80205d3ee7bf7af35b33c7ccf80f395e6b5
Mohammed Fuad Al-nasheri <70831918+Mohmmde1@users.noreply.github.com>
2025-08-07 17:37:26 +0300

    feat: added initial models

--- packetmanager/users/models.py
+++ packetmanager/users/models.py
@@ -1,6 +1,15 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.db.models import PROTECT
 from django.db.models import CharField
+from django.db.models import DateTimeField
+from django.db.models import ForeignKey
+from django.db.models import Model
+from django.db.models import UUIDField
 from django.urls import reverse
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
@@ -24,3 +33,37 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"username": self.username})
+
+class BaseModel(Model):
+    id = UUIDField(default=uuid.uuid4, primary_key=True)
+    created_at = DateTimeField(db_index=True, default=timezone.now)
+    updated_at = DateTimeField(auto_now=True)
+    created_by = ForeignKey(User, on_delete=PROTECT)
+
+
+
+    class Meta:
+        abstract = True
+
+class Client(BaseModel):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(BaseModel):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class StockEntry(BaseModel):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    expiry_date = models.DateField()
+    packet_count = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.client} - {self.product} - {self.expiry_date}"
